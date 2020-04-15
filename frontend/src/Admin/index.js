92face6c823e327db4b619eec54e980cc43864ed
mrbrunelli <matheus.brunelli@gmail.com>
2020-04-15 18:52:22 -0300

    Arrumando sidebar. Componente Link adicionado em Home.

--- frontend/src/Admin/index.js
+++ frontend/src/Admin/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import { Layout, Menu } from 'antd';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd'
+import { Link } from 'react-router-dom'
+import { UserOutlined, HomeOutlined } from '@ant-design/icons'
 import './styles.css'
 
 const { Header, Content, Footer, Sider } = Layout;
@@ -25,22 +26,14 @@ export default function Admin(props) {
                         <div className="break"></div>
                         <small>PHP & ReactJs</small>
                     </div>
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
                         <Menu.Item key="1">
-                            <UserOutlined />
-                            <span className="nav-text">nav 1</span>
+                            <HomeOutlined />
+                            <Link to="/"><span className="nav-text">Home</span></Link>
                         </Menu.Item>
                         <Menu.Item key="2">
-                            <VideoCameraOutlined />
-                            <span className="nav-text">nav 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <UploadOutlined />
-                            <span className="nav-text">nav 3</span>
-                        </Menu.Item>
-                        <Menu.Item key="4">
                             <UserOutlined />
-                            <span className="nav-text">nav 4</span>
+                            <span className="nav-text">Cadastrar</span>
                         </Menu.Item>
                     </Menu>
                 </Sider>
